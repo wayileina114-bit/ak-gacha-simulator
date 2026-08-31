6dcc8215ddcbffccb98eb9d954f2b7ebc8633b18
wayileina114-bit <248518357+wayileina114-bit@users.noreply.github.com>
2026-08-31 20:58:15 +0800

    chore: remove v12.42 zip and temp scripts

--- _m2.js
+++
@@ -1,5 +0,0 @@
-﻿const fs=require('fs');
-const html=fs.readFileSync('抽卡模拟器.html','utf8');
-const i=html.indexOf('干员轮换卡池192');
-console.log('轮换192位置: '+i);
-if(i>=0) console.log('上下文: '+html.slice(Math.max(0,i-100), i+150));
